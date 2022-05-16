e05a06c10160e6528b115ef854fb5b217a408672
pqmEngr <engineer.pqm@gmail.com>
2022-05-16 09:08:27 +0700

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -6,7 +6,7 @@ git clone git@gitlab.com:mkac-agv/agv_tape_config.git -b agv_lidar_test
 git clone git@gitlab.com:mkac-agv/arduino_common_library.git
 git clone git@gitlab.com:mkac-agv/magnetic_agv.git -b develop
 git clone git@gitlab.com:mkac-agv/path_creator.git
-
+git clone git@gitlab.com:mkac-agv/std_stamped_msgs.git
 
 git clone https://github.com/RobotWebTools/rosbridge_suite.git 
 sudo apt-get install ros-melodic-kobuki-* -y
@@ -16,3 +16,4 @@ sudo apt-get install ros-melodic-rosserial-arduino
 sudo apt-get install ros-melodic-rosserial
 
 sudo apt-get install ros-melodic-rosbridge-server
+pip install peewee
